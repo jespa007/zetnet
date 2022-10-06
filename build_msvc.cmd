06cd95f1ae1d23da5893cdec53e696646e9eef26
jespada <jespa007@gmail.com>
2022-10-06 12:42:25 +0200

    update build_msvc

--- build_msvc.cmd
+++ build_msvc.cmd
@@ -9,13 +9,8 @@ SET BINARY_DIR=%ROOT_DIR%bin\msvc\release
 SET TARGET=release
  
 :: The third parties
-SET SDL2_VERSION=SDL2-2.0.14
-SET LIBXMP_VERSION=libxmp-lite-4.5.0
-SET ZETSCRIPT_VERSION=zetscript-2.0.0
-
-SET SDL2_FILE=%SDL2_VERSION%.tar.gz
-SET LIBXMP_FILE=%LIBXMP_VERSION%.tar.gz
-SET ZETSCRIPT_FILE=%ZETSCRIPT_VERSION%.tar.gz
+SET OPENSSL_VERSION=openssl-openssl-3.0.4
+SET OPENSSL_FILE=%OPENSSL_VERSION%.tar.gz
 
 :: Detect which Visual Studio has installed
 IF NOT "%VS160COMNTOOLS%"=="" (
@@ -58,100 +53,45 @@ cd %ROOT_DIR%
 
 ::------------------------------------------------------------------------------------------------------------------------
 ::
-:: SDL2
+:: openssl
 ::
-echo building SDL2...
+echo building openssl...
 
-IF NOT EXIST %THIRD_PARTY_BUILD_DIR%\%SDL2_VERSION%\NUL (
-	tools\7zip\7za.exe x src\3rdparty\%SDL2_FILE% -so| tools\7zip\7za.exe x -aoa -si -ttar -o%THIRD_PARTY_BUILD_DIR%
+IF NOT EXIST %THIRD_PARTY_BUILD_DIR%\%OPENSSL_VERSION%\NUL (
+	tools\7zip\7za.exe x src\3rdparty\%OPENSSL_FILE% -so| tools\7zip\7za.exe x -aoa -si -ttar -o%THIRD_PARTY_BUILD_DIR%
 )
 
-IF NOT EXIST %THIRD_PARTY_LIB%\SDL2.lib (
-	cd %THIRD_PARTY_BUILD_DIR%\%SDL2_VERSION%
-
+IF NOT EXIST %THIRD_PARTY_LIB%\openssl.lib (
+	
+	cd %THIRD_PARTY_BUILD_DIR%\%OPENSSL_VERSION%
 	rd build /S /Q
-	cmake  -H. -Bbuild -G %CMAKE_GENERATOR% -DBUILD_SHARED_LIBS:BOOL=OFF -DFORCE_STATIC_VCRT:BOOL=ON
-	msbuild build\SDL2.sln /t:Rebuild /p:Configuration=Release
+		
+	perl Configure mingw64  --prefix=$THIRD_PARTY_INSTALL_DIR
 	
-	
-	IF NOT EXIST %THIRD_PARTY_INCLUDE%\SDL2 (
-		MD %THIRD_PARTY_INCLUDE%\SDL2
-		copy include\* %THIRD_PARTY_INCLUDE%\SDL2
+	IF NOT EXIST %THIRD_PARTY_INCLUDE%\openssh (
+		MD %THIRD_PARTY_INCLUDE%\openssh
+		copy include\* %THIRD_PARTY_INCLUDE%\openssh
 	)
-	
 	copy build\Release\* %THIRD_PARTY_LIB%
 )
 
 cd %ROOT_DIR%
 
-
 ::------------------------------------------------------------------------------------------------------------------------
 ::
-:: LIBXMP
-::
-echo building libxmp...
-
-IF NOT EXIST %THIRD_PARTY_BUILD_DIR%\%LIBXMP_VERSION%\NUL (
-	tools\7zip\7za.exe x src\3rdparty\%LIBXMP_FILE% -so| tools\7zip\7za.exe x -aoa -si -ttar -o%THIRD_PARTY_BUILD_DIR%
-)
-
-IF NOT EXIST %THIRD_PARTY_LIB%\libxmp-lite.lib (
-	cd %THIRD_PARTY_BUILD_DIR%\%LIBXMP_VERSION%
-
-	nmake /f Makefile.vc clean
-	nmake /f Makefile.vc
-		
-	copy include\libxmp-lite\xmp.h %THIRD_PARTY_INCLUDE%\xmp.h
-	
-	copy libxmp-lite.lib %THIRD_PARTY_LIB%
-)
-
-cd %ROOT_DIR%
-
-::------------------------------------------------------------------------------------------------------------------------
-::
-:: zetscript
-::
-echo building zetscript...
-
-IF NOT EXIST %THIRD_PARTY_BUILD_DIR%\%ZETSCRIPT_VERSION%\NUL (
-	tools\7zip\7za.exe x src\3rdparty\%ZETSCRIPT_FILE% -so| tools\7zip\7za.exe x -aoa -si -ttar -o%THIRD_PARTY_BUILD_DIR%
-)
-
-IF NOT EXIST %THIRD_PARTY_LIB%\zetscript.lib (
-	cd %THIRD_PARTY_BUILD_DIR%\%ZETSCRIPT_VERSION%
-
-	rd build /S /Q
-	cmake  -H. -Bbuild -G %CMAKE_GENERATOR% -DBUILD_SHARED_LIBS:BOOL=OFF -DSUIT_TEST:BOOL=false
-	msbuild build\zetscript.sln /t:Rebuild /p:Configuration=Release
-	
-	
-	IF NOT EXIST %THIRD_PARTY_INCLUDE%\zetscript (
-		MD %THIRD_PARTY_INCLUDE%\zetscript
-		xcopy src\*.h %THIRD_PARTY_INCLUDE%\zetscript /s
-		xcopy src\*.tcc %THIRD_PARTY_INCLUDE%\zetscript /s
-		
-	)
-	
-	copy build\Release\*.lib %THIRD_PARTY_LIB%
-)
-
-cd %ROOT_DIR%
-
-::------------------------------------------------------------------------------------------------------------------------
-::
-:: zg_ge
+:: zetnet
 ::
 
-echo building zs_ge...
+echo building zetnet...
 
-IF NOT EXIST %BINARY_DIR%\zs_ge.exe (
+IF NOT EXIST %THIRD_PARTY_LIB%\zetnet.lib (
 
-	rd build\msvc\zs_ge /S /Q
-	cmake  -H. -Bbuild\msvc\zs_ge -G %CMAKE_GENERATOR%
-	msbuild build\msvc\zs_ge\zs_ge.sln /t:Rebuild /p:Configuration=Release
+	rd build\msvc\zetnet /S /Q
+	cmake  -H. -Bbuild\msvc\zetnet -G %CMAKE_GENERATOR%
+	msbuild build\msvc\zetnet\zetnet.sln /t:Rebuild /p:Configuration=Release
 
-	copy build\msvc\zs_ge\Release\zs_ge.exe %BINARY_DIR%
+	copy build\msvc\zetnet\Release\zetnet.exe %BINARY_DIR%
 )
 
+echo All OK
 
